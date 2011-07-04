d555b2bf3821b3fdab373569652a9e8997f8f828
jack <jack.lightbody@gmail.com>
2011-07-04 04:26:35 -0700

    changed for backwards compatibility

--- web/concrete/helpers/text.php
+++ web/concrete/helpers/text.php
@@ -79,7 +79,7 @@ class TextHelper {
 	 * @param int $maxlength
 	 * @return string
 	 */
-	function sanitize($string, $allowed, $maxlength = 0) {
+	function sanitize($string, $maxlength = 0, $allowed = '') {
 		$text = trim(strip_tags($string, $allowed));
 		if ($maxlength > 0) {
 			if (function_exists('mb_substr')) {
