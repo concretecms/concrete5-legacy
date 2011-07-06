2167bf6c0d8236f9bb87147580e2ba4aa878f8cc
Isaac Jessup <ibjessup@comcast.net>
2011-07-06 12:36:40 -0400

    Added failsafes to the dashboard/bricks/add page

--- web/concrete/controllers/dashboard/bricks/add.php
+++ web/concrete/controllers/dashboard/bricks/add.php
@@ -11,15 +11,44 @@ class DashboardBricksAddController extends Controller {
 		$this->set('subnav', $subnav);
 		if($post = $this->post()) {
 			$txt = Loader::helper('text');
-			if(!$post['package_handle']) $post['package_handle'] = 0;
-			AttributeKeyCategory::add($txt->uncamelcase($txt->camelcase($post['akCategoryName'])), $post['enableSets'], $post['package_handle']);
-			$this->redirect('dashboard/bricks');
+			if(empty($post['akCategoryName'])) {
+				$this->redirect('dashboard/bricks/add/error/malformed');
+			} else {
+				try{
+					if(!$post['package_handle']) $post['package_handle'] = 0;
+					$akc = AttributeKeyCategory::add($txt->uncamelcase($txt->camelcase($post['akCategoryName'])), $post['enableSets'], $post['package_handle']);
+					if($post['associateAttributeTypes'] == '1') {
+						$atypes = AttributeType::getList();
+						foreach($atypes as $type) {
+							$akc->associateAttributeKeyType($type);
+						}
+					}
+					$this->redirect('dashboard/bricks');
+				} catch(Exception $e) {
+					$this->redirect('dashboard/bricks/add/error/exists');
+				}
+			}
 		} else {
 			$this->set('packageHandle', $packageHandle);
 			$form = Loader::helper('form');
 			$this->set('form', $form);
 			$ih = Loader::helper('concrete/interface');
 			$this->set('ih', $ih);
+		}
+	}
+	
+	public function error($msg) {
+		$form = Loader::helper('form');
+		$this->set('form', $form);
+		$ih = Loader::helper('concrete/interface');
+		$this->set('ih', $ih);
+		switch($msg) {
+			case 'exists':
+				$this->set('error', array('Attribute Key Category name already in use.'));
+				break;
+			case 'malformed':
+				$this->set('error', array('Invalid Attribute Key Category name.'));
+				break;
 		}
 	}
 		
--- web/concrete/single_pages/dashboard/bricks/add.php
+++ web/concrete/single_pages/dashboard/bricks/add.php
@@ -1,20 +1,25 @@
 <?php defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
-<div style="width:200px">
+<div style="width:250px">
 	<h1><span>Add Custom Category</span></h1>
 	<div class="ccm-dashboard-inner">
-		<form method="post" action="" id="add-new-category">
+		<form method="post" action="<?php print View::url('dashboard/bricks/add'); ?>" id="add-new-category">
 			<?php print $form->hidden('package_handle', $packageHandle); ?>
-			<?php print $form->label('akCategoryName', 'Category Name'); ?>
+			<h2><?php print $form->label('akCategoryName', 'Category Display Name'); ?></h2>
 			<?php print $form->text('akCategoryName'); ?>
 			<br />
 			<br />
-			<?php print $form->label('enableSets', 'Enable Sets?'); ?>
-			<?php print $form->radio('enableSets', 1). 'Yes'; ?>
-			<?php print $form->radio('enableSets', 0). 'No'; ?>
+			<h2><?php print $form->label('enableSets', 'Enable Sets?'); ?></h2>
+			<?php print $form->radio('enableSets', 1, 1). 'Yes'; ?>
+			<?php print $form->radio('enableSets', 0, 1). 'No'; ?>
+			<br />
+			<br />
+			<h2><?php print $form->label('associateAttributeTypes', 'Associate all Attribute Types?'); ?></h2>
+			<?php print $form->radio('associateAttributeTypes', 1, 1). 'Yes'; ?>
+			<?php print $form->radio('associateAttributeTypes', 0, 1). 'No'; ?>
 			<br />
 			<br />
 			<?php print $ih->submit('Add', 'add-new-category', 'right'); ?>
-			<?php print $ih->button('Cancel', '..', 'left'); ?>
+			<?php print $ih->button('Cancel', View::url('dashboard/bricks'), 'left'); ?>
 		</form>
 	</div>
 </div>
