ce329bca5354b78e8c78d68a3f270a3cb098a96c
Stephen <stephenr85@gmail.com>
2011-09-13 18:05:12 -0400

    Converted the brick item selector to use a custom jQuery UI widget (ccm_attributeKeyCategoryItemSelector). Greatly improves isolation of selector isntances as well as separation of javascript code from configuration for the instance.

--- web/concrete/helpers/form/attribute_key_category_item_selector.php
+++ web/concrete/helpers/form/attribute_key_category_item_selector.php
@@ -15,15 +15,18 @@ defined('C5_EXECUTE') or die("Access Denied.");
 class FormAttributeKeyCategoryItemSelectorHelper {
 	
 	public function selectItems($akCategoryHandle, $fieldName, $values, $akID, $searchInstance = false) {
-		$html = '<input type="hidden" name="'.$fieldName.'[]" value="" />';
-		$html .= '<table width="100%" id="ccmAttributeKeyCategoryItemSelect' . $akID . '" class="ccm-results-list" cellspacing="0" cellpadding="0" border="0">'; 
+		$baseID = uniqid("ccmAttributeKeyCategoryItemSelect$akID_");
+		$itemActionsCell = '<td class="item-actions"><a class="remove" href="javascript:;"><img width="16" height="16" src="' . ASSETS_URL_IMAGES . '/icons/close.png"></a>%input%</td>';
+		$fieldName = $fieldName.'[]';
+		$html = '<div id="'.$baseID.'"><input type="hidden" name="'.$fieldName.'" value="" />';
+		$html .= '<table width="100%" id="'.$baseID.'_table" class="ccm-results-list" cellspacing="0" cellpadding="0" border="0"><thead>'; 
 		Loader::model('attribute_key_category_item_list');
 		$columns = AttributeKeyCategoryColumnSet::getCurrent($akCategoryHandle);
 		if(is_array($columns->getColumns())) foreach($columns->getColumns() as $col) {
 			$html .= '<th>'.$col->getColumnName().'</th>';
 		}
-		$html .= '<th width="30px" align="right"><a class="ccm-attribute-key-category-select-item" dialog-width="90%" dialog-height="70%" dialog-modal="false" dialog-title="' . t('Choose Items') . '" href="' . REL_DIR_FILES_TOOLS_REQUIRED . '/bricks/search_dialog?mode=choose_multiple&akCategoryHandle='.$akCategoryHandle.'&akID='.$akID.'&searchInstance=akID_'.$akID*time().'&fieldName='.$fieldName.'[]"><img src="' . ASSETS_URL_IMAGES . '/icons/add.png" width="16" height="16" /></a></th>';
-		$html .= '</tr><tbody id="ccmAttributeKeyCategoryItemSelect' . $akID . '_body" >';
+		$html .= '<th width="30px" align="right"><a class="ccm-attribute-key-category-select-item" href="javascript:;"><img src="' . ASSETS_URL_IMAGES . '/icons/add.png" width="16" height="16" /></a></th>';
+		$html .= '</tr></thead><tbody id="' . $baseID . '_body" >';
 		if(!empty($values)) {
 			foreach($values as $akci) {
 				if($akci) {
@@ -31,43 +34,33 @@ class FormAttributeKeyCategoryItemSelectorHelper {
 					if(is_array($columns->getColumns())) foreach($columns->getColumns() as $col) {
 						$html .= '<td>'.$col->getColumnValue($akci).'</td>';
 					}
-					$html .= '<td align="center"><input type="hidden" value="'.$akci->ID.'" name="'.$fieldName.'[]"><a class="ccm-attribute-key-category-item-list-clear" href="javascript:void(0)"><img width="16" height="16" class="ccm-attribute-key-category-item-list-clear-button" src="' . ASSETS_URL_IMAGES . '/icons/close.png"></a></td></tr>';
+					$html .= str_replace("%input%", "<input type=\"hidden\" name=\"$fieldName\" value=\"$akci->ID\" />", $itemActionsCell);
 				}
 			}
 		} else {
-			$html .= '<tr class="ccm-attribute-key-category-selected-item-none-'.$akID.'"><td colspan="3">' . t('No items selected.') . '</td></tr>';
+			$html .= '<tr class="ccm-attribute-key-category-selected-item-none"><td colspan="3">' . t('No items selected.') . '</td></tr>';
 		}
-		$html .= '</tbody></table><script type="text/javascript">
-		$(function() {
-			$("#ccmAttributeKeyCategoryItemSelect' . $akID . ' .ccm-attribute-key-category-select-item").dialog();
-			$("a.ccm-attribute-key-category-item-list-clear").click(function() {
-				$(this).parent().parent().remove();
-				ccm_setupGridStriping(\'ccmAttributeKeyCategoryItemSelect' . $akID . '\');
-			});
-		});
+		$html .= '</tbody></table></div>';
+
 		
-		ccm_triggerSelectAttributeKeyCategoryItem = function(akID, that) {
-			$("tr.ccm-attribute-key-category-selected-item-none-"+akID).hide();
-			val = that.children(\':first-child\').find(\'input[name=ID]\').val();
-			if ($("#ccmAttributeKeyCategoryItemSelect"+akID+"_" + val).length < 1) {
-				html = \'<input type="hidden" value="\'+val+\'" name="\'+that.attr(\'fieldName\')+\'"><a class="ccm-attribute-key-category-item-list-clear" href="javascript:void(0)"><img width="16" height="16" class="ccm-attribute-key-category-item-list-clear-button" src="' . ASSETS_URL_IMAGES . '/icons/close.png"></a>\';
-				that.children(":first-child").remove();
-				that.children(":last-child").append(html);
-				that.children(":last-child").attr("align", "center");
-				that.children().removeAttr("onclick");
-				that.attr("id", "ccmAttributeKeyCategoryItemSelect"+akID+"_" + val);
-				$("#ccmAttributeKeyCategoryItemSelect"+akID+"_body").append(that);
-			}
-			
-			ccm_setupGridStriping(\'ccmAttributeKeyCategoryItemSelect"+akID+"\');
-			$("a.ccm-attribute-key-category-item-list-clear").click(function() {
-				$(this).parent().parent().remove();
-				ccm_setupGridStriping(\'ccmAttributeKeyCategoryItemSelect"+akID+"\');
-			});
-		}
-		</script>
+		$html .= '
 		<script type="text/javascript">
-			ccm_setupAttributeKeyCategoryItemSearch(\'akID_'.$akID*time().'\', '.$akID.');
+		
+			$(function(){
+				$("#'.$baseID.'").ccm_attributeKeyCategoryItemSelector({
+					fieldName:"'.$fieldName.'",
+					selectItemDialog:{
+						title:"'. t('Choose Items') .'"
+					},
+					selectItemParameters:{
+						akCategoryHandle:"'.$akCategoryHandle.'",
+						akID:"'.$akID.'",
+						searchInstance:"'.$baseID.'"
+					},
+					itemActionsCell:"'.addslashes(str_replace('%input%', '', $itemActionsCell)).'"
+				});
+			});
+			
 		</script>';	
 		return $html;
 	}
--- web/concrete/js/attribute_key_category.ui.js
+++ web/concrete/js/attribute_key_category.ui.js
@@ -29,7 +29,7 @@ ccm_setupAttributeKeyCategoryItemSearch = function(searchInstance, akID) {
 			switch(action) {
 				case 'choose':
 					$("td.ccm-" + searchInstance + "-list-cb input[type=checkbox]:checked").each(function() {
-						ccm_triggerSelectAttributeKeyCategoryItem(akID, $(this).parent().parent());
+						ccm_triggerSelectAttributeKeyCategoryItem(akID, $(this).closest("tr"));
 					});
 					jQuery.fn.dialog.closeTop();
 					break;
@@ -72,16 +72,211 @@ ccm_setupAttributeKeyCategoryItemSearch = function(searchInstance, akID) {
 			$("#ccm-" + searchInstance + "-advanced-search").submit();
 		});
 	}
-}
+};
 
 ccm_closeModalRefeshSearch = function(searchInstance) {
 	jQuery.fn.dialog.closeTop();
 	$("#ccm-" + searchInstance + "-advanced-search").submit();
-}
+};
 ccm_deleteAndRefeshSearch = function(URIComponents, searchInstance) {
 	$.ajax({
 		url: CCM_TOOLS_PATH + '/bricks/bulk_delete?task=delete&json=' + URIComponents
 	}).responseText;
 	jQuery.fn.dialog.closeTop();
 	$("#ccm-" + searchInstance + "-advanced-search").submit();
-}
+};
+
+
+
+
+(function(){
+	var $ = jQuery,
+		undefined,
+		console;
+		
+	if(typeof window.console === "undefined"){
+		console = {};
+		console.debug = console.info = console.warn = console.error = console.log = function(){};	
+	}else{
+		console = window.console;	
+	}
+	
+	var ccm_attributeKeyCategoryItemSelector = {
+		options:{
+			selectItemDialog:{
+				href:CCM_TOOLS_PATH+"/bricks/search_dialog",
+				width:"90%",
+				height:"70%",
+				modal:false
+			},
+			selectItemParameters:{
+				mode:"choose_multiple"	
+			}
+		},
+		_init:function(){
+			var I = this;
+			
+			console.log(this.options);			
+			
+			this._table = this.element.find("table").first();
+			this._tbody = this._table.children("tbody");
+			
+			//Setup the item list wrapper
+			this._tbody.sortable({axis:"y", delay:300});
+			this._tbody.disableSelection();
+			
+			this._tbody.bind("sortstop", function(event, ui) {
+				I.restripe();
+			  	//Not sure what the best way to relay this is yet, or if we need to at all.
+			});		
+			
+			//Delegate item actions
+			this._tbody.children("tr").each(function(){
+				I.delegateItemActions($(this), $(this));
+			});
+			
+			this.restripe();
+			
+			this.element.bind("", function(evt){
+				console.log(arguments);
+			});
+			
+			//Setup the item selector trigger
+			this.element.find(".ccm-attribute-key-category-select-item").bind("click", function(){
+				I.openItemSelect();
+			});
+			
+		},
+		addItem:function($row, position){
+			
+			var $item = $row,
+				$id = $item.find("input[name=ID]"),
+				$existing = this._tbody.find("input[name='"+ this.options.fieldName +"'][value='"+ $id.val() +"']"); //see if this item already exists
+			
+			if($existing.length){
+				$item = $existing.closest("tr").remove();
+			}else{
+				//Remove uncessary columns
+				var $cols = $item.children("td");
+					
+				$cols.first().remove();
+				$cols.last().remove();
+				$cols.last().remove();
+				$item.append(this.options.itemActionsCell);
+				
+				$item.children("td").last().append("<input type='hidden' name='"+this.options.fieldName+"' value='"+$id.val()+"' />");
+				
+				this.delegateItemActions($item, $item);
+			}
+			
+			if(position < 1){
+				this._tbody.prepend($item);
+			}else if(position == null || position > this._tbody.children("tr").length){
+				this._tbody.append($item);	
+			}else{
+				this._tbody.children("tr").eq(position).insertAfter($item);
+			}
+			
+			var $noneSelected = this._tbody.children(".ccm-attribute-key-category-selected-item-none");
+			if($noneSelected.length){
+				$noneSelected.remove();	
+			}
+			
+			this.restripe();
+			
+			this._trigger('addItem', $row, position);
+		},
+		
+		itemActions:{
+			remove:function(action, $item, position, widget){
+				//widget.removeItem($item.find("input").val());
+				$item.remove();
+			}
+		},
+		/*
+		createItemAction:function(key, callback){
+			
+			var $actions = this.template.find(".item-actions"),
+				$action = $actions.children("."+key);
+			
+			//Add action to template, if it doesn't already exist
+			if(!$action.length){
+				$action = $("<a class=\""+key+"\" title=\""+(key)+"\">"+key+"</a>");
+			}
+			
+			this.itemActions[key] = callback;
+			
+		},
+		*/
+		
+		delegateItemActions:function($item, $actionsWrap){
+			var I = this;
+			$actionsWrap.delegate("a", "click", function(){
+			
+				var $a = $(this);				
+				var classes = $a.attr("class") ? $.trim($a.attr("class")).split(/\s+/g) : [];
+				console.log(this, classes);
+				for(var c = 0; c < classes.length; c++){
+					var key = classes[c],
+						$item = $a.closest("tr");
+					if($.isFunction(I.itemActions[key])){
+						var args = [key, $item, $item.prevAll("tr").length, I],
+							result = I.itemActions[key].apply($item.get(0), args);
+						
+						if(result !== false){
+							args.splice(0,0,key+"Item");
+							I._trigger.apply(I, args);
+						}
+					}
+				}
+				
+			});
+			
+		},
+		
+		openItemSelect:function(opts, params){
+			
+			var parameters = $.extend({fieldName:this.options.fieldName}, this.options.selectItemParameters, params),
+				dialogOpts = $.extend({}, this.options.selectItemDialog, opts);
+				
+			dialogOpts.href += "?"+$.param(parameters);
+			console.log(dialogOpts);
+			
+			this.takeover_ccm_triggerSelectAttributeKeyCategoryItem();
+			jQuery.fn.dialog.open(dialogOpts);
+			//this.element.find(".ccm-attribute-key-category-select-item").click();
+			
+		},
+		
+		restripe:function(){
+			
+			var $items = this._tbody.children("tr");
+			$items.removeClass("ccm-list-record-alt").filter(":odd").addClass("ccm-list-record-alt");
+			this._trigger("restripe");
+		},
+		
+		takeover_ccm_triggerSelectAttributeKeyCategoryItem:function(){
+			//IF THERE ARE ISSUES WITH THE ASSET PICKER COMMUNICATION, LOOK HERE FIRST
+			var I = this,
+				original = typeof(ccm_triggerSelectAttributeKeyCategoryItem)==="undefined" ? undefined : ccm_triggerSelectAttributeKeyCategoryItem;
+			window.ccm_triggerSelectAttributeKeyCategoryItem = function(something, $row){
+				//I.addItem(fileObj.fID, fileObj.title, fileObj.thumbnailLevel2);
+				
+				I.addItem($row);
+				//Restore the original ccm_chooseAsset
+				/* We can't restore it because of the multiple actions dropdown. There will need to be instructions for other integrations to do a similar function takeover
+				if(original !== undefined){
+					window.ccm_triggerSelectAttributeKeyCategoryItem = original;
+				}*/
+			};		
+		},
+		
+	};
+	
+	
+	//Create the widget
+	$.widget("ccm.ccm_attributeKeyCategoryItemSelector", ccm_attributeKeyCategoryItemSelector);
+	
+	
+
+})();
