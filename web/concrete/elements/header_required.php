c1408f28a547c61238bfcf3cf7ee9bdb1a40f8f0
Patrick Heck <patrick@patrickheck.de>
2011-11-29 16:53:16 +0100

    don't encode htmlentities cause they already are. otherwise "how to use the <title> tag" will become "how to use the &lt;title&gt; tag" because internally the "&lt;" was changed to "&amp;lt;"

--- web/concrete/elements/header_required.php
+++ web/concrete/elements/header_required.php
@@ -26,7 +26,6 @@ if ($akt) {
 	$pageTitle = $akt; 
 	?><title><?php echo htmlspecialchars($akt, ENT_COMPAT, APP_CHARSET)?></title>
 <?php } else { 
-	$pageTitle = htmlspecialchars($pageTitle, ENT_COMPAT, APP_CHARSET);
 	?><title><?php echo sprintf(PAGE_TITLE_FORMAT, SITE, $pageTitle)?></title>
 <? } 
 
