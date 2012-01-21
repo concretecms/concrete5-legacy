3105ac20509d064fd3be84b8cbcc742ce300c21d
Brad Beebe <beebs93@gmail.com>
2012-01-20 21:06:05 -0800

    Added "getCollectionAncestor" method to navigation helper.

--- web/concrete/helpers/navigation.php
+++ web/concrete/helpers/navigation.php
@@ -84,5 +84,41 @@ class NavigationHelper {
 	public function getCollectionURL($cObj) {
 		return $this->getLinkToCollection($cObj, true);
 	}
+	
+	
+	/**
+	 * Retrieves a specific ancestor Page object (not including the home page) of any descendant Page object
+	 * 
+	 * @param object Page $cObj - Starting descendant Page object to check from. Omit to use the current Page object.
+	 * @param integer $nth - The nth ancestor to retrieve starting from the descendant page. Omit to retrieve the highest ancestor
+	 * @return object
+	 */
+	public function getCollectionAncestor($cObj = NULL, $nth = NULL){		
+		// Use the current Page object if nothing was passed or it's an invalid Page object
+		if(!is_object($cObj) || !$cObj instanceof Page || $cObj->error) $cObj = Page::getCurrentPage();
 
+		// We return null if we're on the home page, a system page, a master collection or the current highest ancestor
+		if($cObj->cID == HOME_CID || $cObj->isSystemPage() || $cObj->isMasterCollection() || $cObj->cParentID == HOME_CID){
+			return NULL;
+		// We return our current Page object if we haven't requested any ancestors
+		}elseif($nth === 0){
+			return $cObj;
+		// We return the parent Page object if we only want the immediate ancestor
+		}elseif($nth === 1){
+			return Page::getByID($cObj->cParentID);
+		}
+		
+		// If we've made it this far we know our Page object is at least 3rd tier
+		// so our breadcrumb array will have the home page and a 2nd tier Page object at minimum
+		$arrTrail = array();
+		$arrTrail = Loader::helper('navigation')->getTrailToCollection($cObj);
+		// Shift off the last Page object since it's the home page
+		array_pop($arrTrail);
+		
+		// If we requested an ancestor that doesn't exist (or we didn't specify) we choose the highest ancestor Page object to return
+		$intAncestors = count($arrTrail);
+		$nth = (!is_integer($nth) || $nth < 0) || $nth >= $intAncestors ? $intAncestors : $nth;
+		
+		return $arrTrail[($nth - 1)];
+	}
 }
