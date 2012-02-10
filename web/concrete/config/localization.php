e1cba8748b4416d096e2cbd00ce72c0c679ece4e
concrete5 <concrete5@concret5>
2012-02-11 08:55:12 +0900

    /concrete/config/localization.php The initial configuration changed to Japanese

--- web/concrete/config/localization.php
+++ web/concrete/config/localization.php
@@ -2,7 +2,7 @@
 defined('C5_EXECUTE') or die("Access Denied."); 
 
 $u = new User();
-Config::getOrDefine('SITE_LOCALE', 'en_US');
+Config::getOrDefine('SITE_LOCALE', 'ja_JP.UTF8');
 
 if ($u->getUserDefaultLanguage() != '') {
 	define('ACTIVE_LOCALE', $u->getUserDefaultLanguage());
