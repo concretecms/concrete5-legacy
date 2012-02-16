d85d516b0e62cbe0b5df414b25a9a74326afd926
Michael Krasnow <mnkras@gmail.com>
2012-02-16 10:26:08 -0500

    add close button to error dialog.

--- web/concrete/tools/alert.php
+++ web/concrete/tools/alert.php
@@ -5,3 +5,7 @@ Loader::library('view');
 <div id="ccm-popup-alert" class="ccm-ui">
 	<div id="ccm-popup-alert-message" class="alert-message block-message error"></div>
 </div>
+<? $ih = Loader::helper('concrete/interface')?>
+<div class="dialog-buttons">
+	<?=$ih->button_js(t('Close'), 'jQuery.fn.dialog.closeTop()', 'left')?>	
+</div>
