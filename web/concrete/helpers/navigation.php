ea00c1b212c88d24de8f29f696b0d49a74a9f2be
Brad Beebe <beebs93@gmail.com>
2012-02-28 11:21:01 -0800

    Minor update - removed ability to omit the Page object argument (to keep with the other navigation helper methods) - converted some comparison operators to strict - added check for a valid Page object in the initial "if" statement - used "getCollectionParentID" method instead of accessing the object's property directly - minor changes to documentation

--- web/concrete/helpers/navigation.php
+++ web/concrete/helpers/navigation.php
@@ -89,30 +89,27 @@ class NavigationHelper {
 	/**
 	 * Retrieves a specific ancestor Page object (not including the home page) of any descendant Page object
 	 * 
-	 * @param object Page $cObj - Starting descendant Page object to check from. Omit to use the current Page object.
+	 * @param object Page $cObj - Starting descendant Page object to check from
 	 * @param integer $nth - The nth ancestor to retrieve starting from the descendant page. Omit to retrieve the highest ancestor
-	 * @return object
+	 * @return object - Page object
 	 */
-	public function getCollectionAncestor($cObj = NULL, $nth = NULL){		
-		// Use the current Page object if nothing was passed or it's an invalid Page object
-		if(!is_object($cObj) || !$cObj instanceof Page || $cObj->error) $cObj = Page::getCurrentPage();
-
-		// We return null if we're on the home page, a system page, a master collection or the current highest ancestor
-		if($cObj->cID == HOME_CID || $cObj->isSystemPage() || $cObj->isMasterCollection() || $cObj->cParentID == HOME_CID){
+	public function getCollectionAncestor($cObj, $nth = NULL){		
+		// We return null if passed argument is not a valid Page object, if we're on the home page, a system page, a master collection or the current highest ancestor
+		if((!is_object($cObj) || !$oObj instanceof Page || $cObj->error) || $cObj->cID === HOME_CID || $cObj->isSystemPage() || $cObj->isMasterCollection() || $cObj->cParentID === HOME_CID){
 			return NULL;
 		// We return our current Page object if we haven't requested any ancestors
 		}elseif($nth === 0){
 			return $cObj;
 		// We return the parent Page object if we only want the immediate ancestor
 		}elseif($nth === 1){
-			return Page::getByID($cObj->cParentID);
+			return Page::getByID($cObj->getCollectionParentID());
 		}
 		
 		// If we've made it this far we know our Page object is at least 3rd tier
 		// so our breadcrumb array will have the home page and a 2nd tier Page object at minimum
 		$arrTrail = array();
 		$arrTrail = $this->getTrailToCollection($cObj);
-		// Shift off the last Page object since it's the home page
+		// Pop off the last Page object since it's the home page
 		array_pop($arrTrail);
 		
 		// If we requested an ancestor that doesn't exist (or we didn't specify) we choose the highest ancestor Page object to return
