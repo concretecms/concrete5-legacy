4b7cc9aa556b207ebdea205cd6e3fba8f6258841
companyou <contact@companyou.eu>
2013-02-28 08:56:18 +0100

    Update web/concrete/helpers/concrete/upgrade/version_561.php
    Added a image block refresh, so we don't bug out.
    Related to https://github.com/concrete5/concrete5/pull/944#issuecomment-14221151

--- web/concrete/helpers/concrete/upgrade/version_561.php
+++ web/concrete/helpers/concrete/upgrade/version_561.php
@@ -34,6 +34,11 @@ class ConcreteUpgradeVersion561Helper {
 		if (is_object($bt)) {
 			$bt->refresh();
 		}
+		
+		$bt = BlockType::getByHandle('image');
+		if (is_object($bt)) {
+			$bt->refresh();
+		}
 
 	}
 
