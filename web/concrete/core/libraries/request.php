3d6134ad9d32dd15777d76a46d93587cd4aa22ab
acliss19xx <acliss19xx@localhost>
2013-04-29 15:09:12 +0900

    web/concrete/core/libraries/request.php　オーバーライド close #129

--- web/concrete/core/libraries/request.php
+++ web/concrete/core/libraries/request.php
@@ -327,7 +327,7 @@ class Concrete5_Library_Request {
 		// I think the regexps take care of the trimming for us but just to be sure..
 		$cPath = trim($this->cPath, '/');
 		if ($cPath != '') {
-			return '/' . rawurldecode($cPath);
+			return '/' . $cPath;
 		}
 		return '';
 	}
--- web/concrete/libraries/request.php
+++ web/concrete/libraries/request.php
@@ -1,2 +1,11 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
-class Request extends Concrete5_Library_Request {}
+class Request extends Concrete5_Library_Request {
+	public function getRequestCollectionPath() {
+		// I think the regexps take care of the trimming for us but just to be sure..
+		$cPath = trim($this->cPath, '/');
+		if ($cPath != '') {
+			return '/' . rawurldecode($cPath);
+		}
+		return '';
+	}
+}
