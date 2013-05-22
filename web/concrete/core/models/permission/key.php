4446b757211374fbc33befd426c103df36fc2b8d
Michele Locati <michele@locati.it>
2013-05-22 11:49:27 +0200

    Show localized permissions names and descriptions
    Permission names and descriptions are always shown in English (with the
    methods getPermissionKeyName and getPermissionKeyDescription of the
    class Concrete5_Model_PermissionKey).
    This pull request changes these two functions adding a call to t():
    this is safe since users can't edit the permissions via the user
    interface.

--- web/concrete/core/models/permission/key.php
+++ web/concrete/core/models/permission/key.php
@@ -27,7 +27,7 @@ abstract class Concrete5_Model_PermissionKey extends Object {
 	/** 
 	 * Returns the name for this permission key
 	 */
-	public function getPermissionKeyName() { return $this->pkName;}
+	public function getPermissionKeyName() { return t($this->pkName);}
 
 	/** 
 	 * Returns the handle for this permission key
@@ -37,7 +37,7 @@ abstract class Concrete5_Model_PermissionKey extends Object {
 	/** 
 	 * Returns the description for this permission key
 	 */
-	public function getPermissionKeyDescription() { return $this->pkDescription;}
+	public function getPermissionKeyDescription() { return t($this->pkDescription);}
 	
 	/** 
 	 * Returns the ID for this permission key
