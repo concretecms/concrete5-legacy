0f64c54b06df4790cbace29d1e0523b08018b2c4
Patrick Heck <patrick@patrickheck.de>
2013-05-23 12:05:58 +0200

    allow passing of parentID to url_slug tool

--- web/concrete/tools/pages/url_slug.php
+++ web/concrete/tools/pages/url_slug.php
@@ -2,7 +2,14 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 if (Loader::helper('validation/token')->validate('get_url_slug', $_REQUEST['token'])) {
 	Loader::library('3rdparty/urlify');
-	$name = URLify::filter($_REQUEST['name'], 60, LANGUAGE);
+	$lang = LANGUAGE;
+	if (isset($_REQUEST['parentID']) && $multilingual = Package::getByHandle('multilingual') ) {
+		$ms = MultilingualSection::getBySectionOfSite(Page::getByID($_REQUEST['parentID']));
+		if (is_object($ms)) {
+			$lang = $ms->getLanguage();
+		}
+	}
+	$name = URLify::filter($_REQUEST['name'], 60, $lang);
 
 	$ret = Events::fire('on_page_urlify', $_REQUEST['name']);
 	if ($ret) {
