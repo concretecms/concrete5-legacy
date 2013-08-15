90da5c6b65ad6727d1ab4989ce49524f5e7f6a43
Mike Lay <mike@mkly.io>
2013-08-15 11:07:17 -0700

    Set Page Object to page_forbidden
    Can cause issues with Area rendering when displayin page forbidden
    
    - Set Page object to '/page_forbidden' when rendering the page forbidden
    page.

--- web/concrete/dispatcher.php
+++ web/concrete/dispatcher.php
@@ -196,7 +196,7 @@
 			switch($cp->getError()) {
 				case COLLECTION_FORBIDDEN:
 					$v = View::getInstance();
-					$v->setCollectionObject($c);
+					$v->setCollectionObject(Page::getByPath('/page_forbidden'));
 					$v->render('/page_forbidden');
 					break;
 			}
