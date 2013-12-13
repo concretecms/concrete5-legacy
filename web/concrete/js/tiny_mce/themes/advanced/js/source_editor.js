fffc44146b2969165948b4f5aea008cbdd0373d3
Michele Locati <michele@locati.it>
2013-12-13 18:04:46 +0100

    Add CodeMirror to "View source" of TinyMCE

--- web/concrete/js/tiny_mce/themes/advanced/js/source_editor.js
+++ web/concrete/js/tiny_mce/themes/advanced/js/source_editor.js
@@ -2,10 +2,14 @@ tinyMCEPopup.requireLangPack();
 tinyMCEPopup.onInit.add(onLoadInit);
 
 function saveContent() {
+	if(codeMirror) {
+		codeMirror.save();
+	}
 	tinyMCEPopup.editor.setContent(document.getElementById('htmlSource').value, {source_view : true});
 	tinyMCEPopup.close();
 }
 
+var codeMirror = null;
 function onLoadInit() {
 	tinyMCEPopup.resizeToInnerSize();
 
@@ -21,23 +25,48 @@ function onLoadInit() {
 	}
 
 	resizeInputs();
+	loadCodeMirror(
+		'../../../codemirror',
+		['xml', 'javascript', 'css', 'htmlmixed', 'htmlembedded'],
+		function() {
+			codeMirror = CodeMirror.fromTextArea(
+				document.getElementById('htmlSource'),
+				{
+					lineNumbers: true,
+					mode: 'application/x-ejs',
+					indentUnit: 4,
+					indentWithTabs: true,
+					enterMode: 'keep',
+					tabMode: 'shift'
+				}
+			);
+			resizeInputs();
+			toggleWordWrap()
+		}
+	);
 }
 
 function setWrap(val) {
-	var v, n, s = document.getElementById('htmlSource');
-
-	s.wrap = val;
-
-	if (!tinymce.isIE) {
-		v = s.value;
-		n = s.cloneNode(false);
-		n.setAttribute("wrap", val);
-		s.parentNode.replaceChild(n, s);
-		n.value = v;
+	if(codeMirror) {
+		codeMirror.setOption('lineWrapping', val != 'off');
+	}
+	else {
+		var v, n, s = document.getElementById('htmlSource');
+	
+		s.wrap = val;
+	
+		if (!tinymce.isIE) {
+			v = s.value;
+			n = s.cloneNode(false);
+			n.setAttribute("wrap", val);
+			s.parentNode.replaceChild(n, s);
+			n.value = v;
+		}
 	}
 }
 
-function toggleWordWrap(elm) {
+function toggleWordWrap() {
+	var elm = document.getElementById('wraped');
 	if (elm.checked)
 		setWrap('soft');
 	else
@@ -45,12 +74,50 @@ function toggleWordWrap(elm) {
 }
 
 function resizeInputs() {
-	var vp = tinyMCEPopup.dom.getViewPort(window), el;
+	var vp = tinyMCEPopup.dom.getViewPort(window), width = Math.max(50, vp.w - 20), height = Math.max(50, vp.h - 100);
 
-	el = document.getElementById('htmlSource');
+	if(codeMirror) {
+		codeMirror.setSize(null, height);
+	}
+	else {
+		var el = document.getElementById('htmlSource');
+		el.style.width =  + 'px';
+		el.style.height =  + 'px';
+	}
+}
 
-	if (el) {
-		el.style.width = (vp.w - 20) + 'px';
-		el.style.height = (vp.h - 65) + 'px';
+function loadCodeMirror(codeMirrorPath, modes, callback) {
+	function loadModes() {
+		var scripts = [];
+		if(modes) {
+			$.each(modes, function(_, mode) {
+				if(!(mode in CodeMirror.modes)) {
+					switch(mode) {
+						default:
+							scripts.push(codeMirrorPath + '/mode/' + mode + '/' + mode + '.js');
+							break;
+					}
+				}
+			});
+		}
+		if(!scripts.length) {
+			callback();
+			return;
+		}
+		require(scripts, function() {
+			callback();
+		});
+	}
+	function loadMain() {
+		$('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', codeMirrorPath + '/lib/codemirror.css'));
+		require([codeMirrorPath + '/lib/codemirror.js'], function() {
+			loadModes();
+		});
+	}
+	if($.type(window.CoreMirror) != 'function') {
+		loadMain();
+	}
+	else {
+		loadModes();
 	}
 }
