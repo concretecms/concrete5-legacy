8a83ec426fd63d9c23e69b91fce41ae6fa808086
Michele Locati <michele@locati.it>
2014-02-14 15:17:58 +0100

    Log PHP fatal errors
    We already have the option to log unhandled exceptions.
    Let's log also PHP fatal errors.

--- web/concrete/startup/exceptions.php
+++ web/concrete/startup/exceptions.php
@@ -21,3 +21,24 @@ function Concrete5_Exception_Handler($e) {
 }
 
 set_exception_handler('Concrete5_Exception_Handler');
+
+function Concrete5_Shutdown_Handler() {
+	$lastError = error_get_last();
+	if(is_array($lastError)) {
+		switch($lastError['type']) {
+			case E_ERROR:
+				if (ENABLE_LOG_ERRORS) {
+					$db = Loader::db();
+					$tables = $db->MetaTables();
+					if (in_array('Logs', $tables)) {
+						$l = new Log(LOG_TYPE_EXCEPTIONS, true, true);
+						$l->write(t('Fatal Error Occurred: ') . sprintf("%s:%d %s (%s)\n", $lastError['file'], $lastError['line'], $lastError['message'], 'E_ERROR'));
+						$l->close();
+					}
+				}
+				break;
+		}
+	}
+}
+
+register_shutdown_function('Concrete5_Shutdown_Handler');
