776c7cd09f96be1f5e2bd7809961daa5b40f2c6d
Michele Locati <michele@locati.it>
2014-02-14 19:16:37 +0100

    Switch to standard/overridable library

---
+++ web/concrete/libraries/problems_handler.php
@@ -0,0 +1,5 @@
+<?php defined('C5_EXECUTE') or die('Access Denied.');
+
+/** Helper class to handle unhandled errors/exceptions */
+class ProblemsHandler extends Concrete5_Library_ProblemsHandler {
+}
