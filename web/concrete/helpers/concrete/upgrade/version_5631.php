a35030904d6effe6b7559b42aa974f6758b22cb3
Michele Locati <michele@locati.it>
2014-04-03 09:00:56 +0200

    Add optional balloon to Google Maps block
    Let's allow users to add a balloon to the Google Maps block.

--- web/concrete/helpers/concrete/upgrade/version_5631.php
+++ web/concrete/helpers/concrete/upgrade/version_5631.php
@@ -1,12 +1,16 @@
-<?
+<?php defined('C5_EXECUTE') or die("Access Denied.");
 
-defined('C5_EXECUTE') or die("Access Denied.");
 class ConcreteUpgradeVersion5631Helper {
 
 	public $dbRefreshTables = array(
 		'Users'
 	);
 
-	public function run() {}
+	public function run() {
+		$bt = BlockType::getByHandle('google_map');
+		if(is_object($bt) && (!$bt->isError())) {
+			$bt->refresh();
+		}
+	}
 
 }
