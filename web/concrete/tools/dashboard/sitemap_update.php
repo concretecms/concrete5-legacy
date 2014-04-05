aeb30b07301d2bf753666595c873efa1080d8dfe
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2014-04-05 21:57:39 +0300

    An effort to fix a sorting problem when the system pages are not included in the sorting request.

--- web/concrete/tools/dashboard/sitemap_update.php
+++ web/concrete/tools/dashboard/sitemap_update.php
@@ -6,10 +6,33 @@ if (!$sh->canRead()) {
 }
 
 if (isset($_REQUEST['cID']) && is_array($_REQUEST['cID'])) {
+	
+	// Find out if there are any system pages before the first page to be sorted.
+	// If there are, preserve their original order to keep them in correct 
+	// positions if the system pages are not included in the search request
+	$displayOrderIncrement = 0;
+	$first = Page::getByID(current($_REQUEST['cID']));
+	
+	if (!$first->isSystemPage()) {
+		$db = Loader::db();
+		
+		// Find out the display order of the first non-system page
+		$firstOrder = $db->GetOne("SELECT cDisplayOrder FROM Pages WHERE cIsSystemPage = 0 AND cParentID = ? ORDER BY cDisplayOrder", array($first->getCollectionParentID()));
+		
+		if ($firstOrder > 0) {
+			// And if it does not start the list, find out how many system pages there are before that and
+			// use their maximum display order as the display order increment for the pages to be updated.
+			$row = $db->GetRow("SELECT COUNT(cID) AS cnt, MAX(cDisplayOrder) AS maxOrder FROM Pages WHERE cParentID = ? AND cIsSystemPage = 1 AND cDisplayOrder < ?", array($first->getCollectionParentID(), $firstOrder));
+			if ($row['cnt'] > 0) {
+				$displayOrderIncrement = 1+$row['maxOrder'];
+			}
+		}
+	}
+	
 	foreach($_REQUEST['cID'] as $displayOrder => $cID) { 
-		$v = array($displayOrder, $cID);
+		//$v = array($displayOrder, $cID);
 		$c = Page::getByID($cID);
-		$c->updateDisplayOrder($displayOrder,$cID);
+		$c->updateDisplayOrder($displayOrder+$displayOrderIncrement,$cID);
 	}
 }
 
@@ -18,4 +41,3 @@ $json['message'] = t("Display order saved.");
 $js = Loader::helper('json');
 print $js->encode($json);
 
-?>
