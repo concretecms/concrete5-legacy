417d4beddeea13ef2d63058c12281a4b6923c7a8
Mike Lay <mike@mkly.io>
2014-04-14 10:45:58 -0700

    Add PHPDoc to RouteCallback cleanup whitespace

--- web/concrete/core/libraries/routing/route/callback/closure.php
+++ web/concrete/core/libraries/routing/route/callback/closure.php
@@ -2,16 +2,21 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 class Concrete5_Library_ClosureRouteCallback extends RouteCallback {
 
+	/**
+	 * @return Response
+	 */
 	public function execute(Request $request, Route $route, $parameters) {
 		$r = new Response();
 		$r->setContent($this->callback->__invoke());
 		return $r;
 	}
 
+	/**
+	 * @return array
+	 */
 	public static function getRouteAttributes($callback) {
 		$callback = new ClosureRouteCallback($callback);
 		return array('callback' => $callback);
 	}
-
 
 }
