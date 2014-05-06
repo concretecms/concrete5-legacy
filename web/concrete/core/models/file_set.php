f26c3a5a17c445bf80cc23a5ecffd6f7f5c761ca
Remo Laubacher <remo.laubacher@gmail.com>
2014-05-06 14:54:51 +0200

    set default timestamp value

--- web/concrete/core/models/file_set.php
+++ web/concrete/core/models/file_set.php
@@ -375,7 +375,7 @@
 				$file_set_file->fsfID = null;
 				$file_set_file->fID =  $f_id;			
 				$file_set_file->fsID = $fs_id;
-				$file_set_file->timestamp = null;
+				$file_set_file->timestamp = date('Y-m-d H:i:s');
 				$file_set_file->fsDisplayOrder = $fsDisplayOrder;
 				$file_set_file->Save();
 				return $file_set_file;
