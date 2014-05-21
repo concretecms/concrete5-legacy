b70eda0b1dfd5e7bc8c9f9b7bf92d4fa3b022ffe
Michele Locati <michele@locati.it>
2014-05-21 09:31:17 +0200

    Bugfix

--- web/concrete/tools/files/duplicate.php
+++ web/concrete/tools/files/duplicate.php
@@ -99,7 +99,7 @@ if (!is_array($_REQUEST['fID'])) {
 				<tr>
 					<td><?=$fv->getType()?></td>
 					<td class="ccm-file-list-filename" width="100%"><div style="width: 150px; word-wrap: break-word"><?=$fv->getTitle()?></div></td>
-					<td><?=<?=$dh->formatSpecial('DASHBOARD_SEARCH_RESULTS_FILES', $f->getDateAdded())?></td>
+					<td><?=$dh->formatSpecial('DASHBOARD_SEARCH_RESULTS_FILES', $f->getDateAdded())?></td>
 					<td><?=$fv->getSize()?></td>
 					<td><?=$fv->getAuthorName()?></td>
 				</tr>
