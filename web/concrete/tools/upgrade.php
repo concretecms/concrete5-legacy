1042cc3915634587f5eeb7cbcc771855abc60b43
Aki Ojalehto <aki@ojalehto.eu>
2014-05-27 16:33:45 +0300

    Fix unauthorized upgrade and version information disclosure

--- web/concrete/tools/upgrade.php
+++ web/concrete/tools/upgrade.php
@@ -1,5 +1,11 @@
 <?
 defined('C5_EXECUTE') or die("Access Denied.");
+
+$tp = new TaskPermission();
+if ( ! $tp->canBackup()) {
+	die('Access Denied.');
+}
+
 //not working? try $_GET['force']=1
 $v = View::getInstance();
 $v->setTheme('concrete');
