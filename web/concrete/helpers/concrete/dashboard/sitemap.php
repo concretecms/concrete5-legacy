concretecms/concrete5-legacy

commit a1d859f4b343b01d5fac437b2760195e8edb63cb

Better sanitize values

web/concrete/helpers/concrete/dashboard/sitemap.php
@@ -164,55 +164,58 @@ class ConcreteDashboardSitemapHelper {
 	function getSubNodes($cID, $level = 0, $keywords = '', $autoOpenNodes = true) {
 		$db = Loader::db();
 		
-		$obj = new stdClass;
-		if ($keywords != '' && $keywords != false) {
-			$nc = Page::getByID($cID, 'RECENT');
-			$pl = new PageList();
-			if (PERMISSIONS_MODEL != 'simple') {
-				$pl->setViewPagePermissionKeyHandle('view_page_in_sitemap');
+		if (isset($cID) && Loader::helper('validation/numbers')->integer($cID)) {
+			
+			$obj = new stdClass;
+			if ($keywords != '' && $keywords != false) {
+				$nc = Page::getByID($cID, 'RECENT');
+				$pl = new PageList();
+				if (PERMISSIONS_MODEL != 'simple') {
+					$pl->setViewPagePermissionKeyHandle('view_page_in_sitemap');
+				}
+				$obj->keywords = $keywords;
+				$pl->filterByName($keywords);
+				$pl->ignoreAliases();
+				$pl->filterByPath($nc->getCollectionPath());
+				$pl->displayUnapprovedPages();
+				$pl->sortByDisplayOrder();
+				$results = $pl->get(SITEMAP_PAGES_LIMIT);
+				$total = $pl->getTotal();
+			} else {			
+				$pl = new PageList();
+				if (PERMISSIONS_MODEL != 'simple') {
+					$pl->setViewPagePermissionKeyHandle('view_page_in_sitemap');
+				}
+				$pl->sortByDisplayOrder();
+				if (ConcreteDashboardSitemapHelper::showSystemPages()) {
+					$pl->includeSystemPages();
+					$pl->includeInactivePages();
+				}
+				$pl->filterByParentID($cID);
+				$pl->displayUnapprovedPages();
+				$total = $pl->getTotal();
+				if ($cID == 1) {
+					$results = $pl->get();			
+				} else {
+					$pl->setItemsPerPage(SITEMAP_PAGES_LIMIT);
+					$results = $pl->getPage();
+				}
 			}
-			$obj->keywords = $keywords;
-			$pl->filterByName($keywords);
-			$pl->ignoreAliases();
-			$pl->filterByPath($nc->getCollectionPath());
-			$pl->displayUnapprovedPages();
-			$pl->sortByDisplayOrder();
-			$results = $pl->get(SITEMAP_PAGES_LIMIT);
-			$total = $pl->getTotal();
-		} else {			
-			$pl = new PageList();
-			if (PERMISSIONS_MODEL != 'simple') {
-				$pl->setViewPagePermissionKeyHandle('view_page_in_sitemap');
+			
+			$nodes = array();
+			foreach($results as $c) {
+				$n = ConcreteDashboardSitemapHelper::getNode($c, $level+1, $autoOpenNodes);
+				if ($n != false) {
+					$nodes[] = $n;
+				}
 			}
-			$pl->sortByDisplayOrder();
-			if (ConcreteDashboardSitemapHelper::showSystemPages()) {
-				$pl->includeSystemPages();
-				$pl->includeInactivePages();
-			}
-			$pl->filterByParentID($cID);
-			$pl->displayUnapprovedPages();
-			$total = $pl->getTotal();
-			if ($cID == 1) {
-				$results = $pl->get();			
-			} else {
-				$pl->setItemsPerPage(SITEMAP_PAGES_LIMIT);
-				$results = $pl->getPage();
-			}
+			
+			$obj->total = $total;
+			$obj->nodeID = $cID;
+			$obj->pageList = $pl;
+			$obj->results = $nodes;
+			return $obj;
 		}
-		
-		$nodes = array();
-		foreach($results as $c) {
-			$n = ConcreteDashboardSitemapHelper::getNode($c, $level+1, $autoOpenNodes);
-			if ($n != false) {
-				$nodes[] = $n;
-			}
-		}
-		
-		$obj->total = $total;
-		$obj->nodeID = $cID;
-		$obj->pageList = $pl;
-		$obj->results = $nodes;
-		return $obj;
 	}
 	
 	public function getPermissionsNodes($obj) {
@@ -269,7 +272,7 @@ class ConcreteDashboardSitemapHelper {
 			$this->html .= '</ul></div>';
 		}
 		if ($display_mode == 'full' || $display_mode == '') {
-			$this->html .= '<div class="dropzone tree-dz' . $nodeID . '" tree-parent="' . $nodeID . '" id="tree-dz' . $nodeID . '-sub"></div>';
+			$this->html .= '<div class="dropzone tree-dz' . h($nodeID) . '" tree-parent="' . h($nodeID) . '" id="tree-dz' . h($nodeID) . '-sub"></div>';
 		}
 		$moveableClass = '';
 		for ($i = 0; $i < count($req->results); $i++) {
@@ -296,7 +299,7 @@ class ConcreteDashboardSitemapHelper {
 				$canDrag = "false";
 			}*/
 			
-			$this->html .= '<li ' . $this->getPermissionsNodes($ri) . ' tree-node-intrash="' . $ri['isInTrash'] . '" tree-node-istrash="' . $ri['isTrash'] . '" tree-node-cancompose="' . $ri['canCompose'] . '" tree-node-type="' . $treeNodeType . '" draggable="' . $canDrag . '" class="tree-node ' . $typeClass . ' tree-branch' . $nodeID . '" id="tree-node' . $ri['id'] . '"' . $customIconSrc . '>';
+			$this->html .= '<li ' . $this->getPermissionsNodes($ri) . ' tree-node-intrash="' . $ri['isInTrash'] . '" tree-node-istrash="' . $ri['isTrash'] . '" tree-node-cancompose="' . $ri['canCompose'] . '" tree-node-type="' . $treeNodeType . '" draggable="' . $canDrag . '" class="tree-node ' . $typeClass . ' tree-branch' . h($nodeID) . '" id="tree-node' . $ri['id'] . '"' . $customIconSrc . '>';
 			
 			if ($ri['numSubpages'] > 0) {
 				$subPageStr = ($ri['id'] == 1) ? '' : ' <span class="ccm-sitemap-num-subpages">(' . $ri['numSubpages'] . ')</span>';
@@ -360,7 +363,7 @@ class ConcreteDashboardSitemapHelper {
 			
 			$this->html .= '</li>';
 			if ($display_mode == 'full' || $display_mode == '') {
-				$this->html .= '<div class="dropzone tree-dz' . $nodeID . '" tree-parent="' . $nodeID . '" id="tree-dz' . $ri['id'] . '"></div>';
+				$this->html .= '<div class="dropzone tree-dz' . h($nodeID) . '" tree-parent="' . h($nodeID) . '" id="tree-dz' . $ri['id'] . '"></div>';
 			}
 		}
 		

web/concrete/single_pages/dashboard/pages/single.php
@@ -16,7 +16,7 @@ echo Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Sing
 			<label for="pageURL" class="control-label"><?php echo t('The page you want to add is available at:')?></label>
 			<div class="controls">
 			<div class="input-prepend">
-				<span class="add-on"><?php echo $base?>/</span><input type="text" name="pageURL" value="<?php echo $this->post('pageURL')?>" class="span4" />
+				<span class="add-on"><?php echo $base?>/</span><?php echo $form->text('pageURL', $this->post('pageURL'), array('class'=>'span4')); ?>
 			</div>
 				<button class="btn" type="submit"><?=t('Add')?></button>
 			</div>

web/concrete/single_pages/dashboard/system/attributes/sets.php
@@ -155,7 +155,7 @@ $txt = Loader::helper('text');?>
 	
 	<h3><?=t('Add Set')?></h3>
 
-	<input type="hidden" name="categoryID" value="<?php echo $categoryID?>" />
+	<?php echo $form->hidden('categoryID', intval($categoryID)); ?>
 	<?php echo Loader::helper('validation/token')->output('add_set')?>
 	<div class="clearfix">
 		<?php echo $form->label('asHandle', t('Handle'))?>

web/concrete/single_pages/dashboard/users/add_group.php
@@ -19,7 +19,7 @@ $form = Loader::helper('form');
 <div class="control-group">
 <?=$form->label('gName', t('Name'))?>
 <div class="controls">
-	<input type="text" name="gName" class="span6" value="<?=htmlentities($_POST['gName'])?>" />
+	<input type="text" name="gName" class="span6" value="<?=Loader::helper('text')->entities($_POST['gName'])?>" />
 </div>
 </div>
 

web/concrete/single_pages/dashboard/users/group_sets.php
@@ -127,7 +127,6 @@ defined('C5_EXECUTE') or die("Access Denied.");
 	
 	<h3><?=t('Add Set')?></h3>
 
-	<input type="hidden" name="categoryID" value="<?php echo $categoryID?>" />
 	<?php echo Loader::helper('validation/token')->output('add_set')?>
 	<div class="control-group">
 		<?php echo $form->label('gsName', t('Name'))?>

web/concrete/single_pages/dashboard/users/groups.php
@@ -65,7 +65,7 @@ if ($tp->canAccessGroupSearch()) { ?>
 <? $form = Loader::helper('form'); ?>
 <?=$form->label('gKeywords', t('Keywords'))?>
 <div class="controls">
-	<input type="text" name="gKeywords" value="<?=htmlentities($_REQUEST['gKeywords'])?>"  />
+	<input type="text" name="gKeywords" value="<?=Loader::helper('text')->entities($_REQUEST['gKeywords'])?>"  />
 	<input class="btn" type="submit" value="<?=t('Search')?>" />
 </div>
 <input type="hidden" name="group_submit_search" value="1" />

commit 183be74418ae895a05026cae8057499169b70e70

Fixing broken sitemap selector (broken in commit a1d859f4b343b01d5fac437b2760195e8edb63cb)

web/concrete/helpers/concrete/dashboard/sitemap.php
@@ -165,7 +165,7 @@ class ConcreteDashboardSitemapHelper {
 		$db = Loader::db();
 		
 		$obj = new stdClass;
-		if (isset($cID) && Loader::helper('validation/numbers')->integer($cID)) {
+		if (isset($cID) && (Loader::helper('validation/numbers')->integer($cID) || $cID == 0)) {
 			if ($keywords != '' && $keywords != false) {
 				$nc = Page::getByID($cID, 'RECENT');
 				$pl = new PageList();