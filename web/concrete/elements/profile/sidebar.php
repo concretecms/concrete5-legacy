09b9ffc8511ba7b1c1c6c675e3ae4e013d3062ec
Korvin Szanto <korvinszanto@gmail.com>
2015-10-31 11:48:30 -0700

    https://hackerone.com/reports/9904

--- web/concrete/elements/profile/sidebar.php
+++ web/concrete/elements/profile/sidebar.php
@@ -1,4 +1,9 @@
-<? $av = Loader::helper('concrete/avatar'); ?>
+<?php
+$av = Loader::helper('concrete/avatar');
+
+/** @type ValidationTokenHelper $token */
+$token = Loader::helper('validation/token');
+?>
 <div id="ccm-profile-sidebar">
 	<div class="ccm-profile-header">
 		<a href="<?=View::url('/profile',$profile->getUserID())?>"><?= $av->outputUserAvatar($profile)?></a><br />
@@ -6,33 +11,33 @@
 	</div>
 	<div style="margin-top:16px; padding-bottom:4px; margin-bottom:0px; font-weight:bold"><?=t('Member Since')?></div>
 	<?=Loader::helper('date')->formatDate($profile->getUserDateAdded(), true)?>
-	
-	<? 
+
+	<?
 	$u = new User();
 	if ($u->isRegistered() && $u->getUserID() != $profile->getUserID()) { ?>
 	<div style="margin-top:16px;">
 		<div>
 		<? if( !UsersFriends::isFriend( $profile->getUserID(), $u->uID ) ){ ?>
-			<a href="<?=View::url('/profile/friends','add_friend', $profile->getUserID())?>">
+			<a href="<?=View::url('/profile/friends','add_friend', $profile->getUserID(), $token->generate('profile.add_friend.' . $profile->getUserID()))?>">
 				<?=t('Add to My Friends') ?>
 			</a>
 		<? }else{ ?>
-			<a href="<?=View::url('/profile/friends','remove_friend', $profile->getUserID() )?>">
+			<a href="<?=View::url('/profile/friends','remove_friend', $profile->getUserID(), $token->generate('profile.remove_friend.' . $profile->getUserID()))?>">
 				<?=t('Remove from My Friends') ?>
 			</a>
 		<? } ?>
-		
+
 		</div>
 		<? if ($profile->getUserProfilePrivateMessagesEnabled() == 1) { ?>
-			<a href="<?=$this->url('/profile/messages', 'write', $profile->getUserID())?>"><?=t('Send Private Message')?></a>	
+			<a href="<?=$this->url('/profile/messages', 'write', $profile->getUserID())?>"><?=t('Send Private Message')?></a>
 		<? } ?>
-		
+
 	</div>
 	<? } ?>
 
-	
+
 	<div>
-	<? 
+	<?
 	if($u->getUserID() == $profile->getUserID()) {
 		$nc = Page::getByPath('/profile');
 		$pl = new PageList();
@@ -60,7 +65,7 @@
 		print '&nbsp;&nbsp;';
 		print $form->submit('submit', t('Search'));
 		?>
-		
+
 		</form>
 
 </div>
