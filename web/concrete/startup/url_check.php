b572e4b87ef3dc552e051eaedbe2df78b69d38b1
Andrew Embler <andrew@concrete5.org>
2017-03-20 11:36:45 -0700

    removing translations for the moment, fixing url check

--- web/concrete/startup/url_check.php
+++ web/concrete/startup/url_check.php
@@ -9,10 +9,15 @@ if (REDIRECT_TO_BASE_URL == true && C5_ENVIRONMENT_ONLY == false) {
 		$protocol = 'https://';
 		if (defined('BASE_URL_SSL')) {
 			$base_url_ssl = BASE_URL_SSL;
-		} else { 
-			$base_url_ssl = Config::get('BASE_URL_SSL');
+		} else {
+			// No real way to get around this: we want to check the URL before the page cache check, but
+			// we also want to be able to grab the URL.
+			// So right now we are getting rid of this config check. @TODO –split these checks into separate
+			// files so that they can be checked properly, one before the full page cache check, one after when we have
+			// the database.
+//			$base_url_ssl = Config::get('BASE_URL_SSL');
 		}
-		if ($base_url_ssl) {
+		if (isset($base_url_ssl)) {
 			$base_url = $base_url_ssl;
 		}
 	}
